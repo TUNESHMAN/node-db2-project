129cad0903336587e7ed3c288aa6a11d74df6e2e
Babatunde <babatundea15@gmail.com>
2020-05-21 18:13:49 +0100

    I fixed the endpoint for getting all cars

--- cars/carsDb.js
+++ cars/carsDb.js
@@ -1,2 +1,15 @@
 // Bring in the db
 const db = require("../data/db-config");
+
+module.exports = {
+  getCar,
+//   getCarById,
+//   updateCar,
+//   deleteCar,
+//   addCar,
+};
+
+function getCar() {
+  // This is the equivalent of SELECT * FROM cars
+  return db("cars");
+}
--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -8,8 +8,19 @@ const car = require("../cars/carsDb");
 const router = express.Router();
 
 // The cars endpoints below
+
+// Endpoint to get all cars
 router.get("/", (req, res) => {
-  res.json({ message: `Hello` });
+  car
+    .getCar()
+    .then((car) => {
+      res.status(200).json(car);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ message: `Error retrieving car details`, stack: error.stack });
+    });
 });
 
 // I exported the router to be seen on the server
