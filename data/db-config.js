a6d66e3e27e2696f7d25139f29a1f3fae9e1e9fc
Babatunde <babatundea15@gmail.com>
2020-05-21 09:04:49 +0100

    Created dotenv file and configured pg

---
+++ data/db-config.js
@@ -0,0 +1,6 @@
+// This is the file that has knowledge of knex. It is where we bring in our configuration whether development or staging.
+// It is also where we expose the db utility to the outer world.
+const knex = require("knex");
+const configuration = require("../knexfile").development;
+
+module.exports = knex(configuration);
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+server.use(logger);
 
 // Flesh out a dummy API
 server.get("/", (req, res) => {
@@ -19,5 +20,14 @@ server.get("*", (req, res) => {
   res.send({ message: `Sorry, this is an invalid path` });
 });
 
+// Building a logger middleware
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
+  next();
+}
 // Export the server to the outer world
 module.exports = server;
