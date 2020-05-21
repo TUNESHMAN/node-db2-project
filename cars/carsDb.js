0505e5194245a38f25e12ac62790789233ca4fdc
Babatunde <babatundea15@gmail.com>
2020-05-21 19:26:26 +0100

    I fixed the endpoint to get a car by id

--- cars/carsDb.js
+++ cars/carsDb.js
@@ -3,13 +3,19 @@ const db = require("../data/db-config");
 
 module.exports = {
   getCar,
-//   getCarById,
-//   updateCar,
-//   deleteCar,
-//   addCar,
+  getCarById,
+  //   updateCar,
+  //   deleteCar,
+  //   addCar,
 };
 
+// Helper function for retrieving all cars
 function getCar() {
   // This is the equivalent of SELECT * FROM cars
   return db("cars");
+}
+
+// Helper function to get a particular car by its id
+function getCarById(id) {
+  return db("cars").where({ id }).first();
 }
--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -23,5 +23,34 @@ router.get("/", (req, res) => {
     });
 });
 
+// Endpoint to get a car by its id
+router.get("/:id", validateCarId, (req, res) => {});
+
+// Middleware for validating a car by id
+function validateCarId(req, res, next) {
+  const { id } = req.params; //The id comes from req.params
+  // The id may exist or not
+  car
+    .getCarById(id)
+    .then((car) => {
+      if (car) {
+        console.log(car);
+
+        res.status(200).json(car);
+      } else if (!car) {
+        res
+          .status(404)
+          .json({ message: `The car with the id ${id} does not exist` });
+      } else {
+        next();
+      }
+    })
+    .catch((error) => {
+      console.log(c);
+
+      res.status(500).json({ message: error.message, stack: error.stack });
+    });
+}
+
 // I exported the router to be seen on the server
 module.exports = router;
